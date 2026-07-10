40193a4ba285d831e7b1af3d156470a5c9d3a338
Vijay <itagikarv@gmail.com>
2026-07-10 19:46:28 +0530

    bio.tsx file added

---
+++ app/analysis/bio.tsx
@@ -0,0 +1,27 @@
+import React, { memo } from 'react'
+import { connect } from 'react-redux'
+
+interface Props {}
+
+function Bio(props: Props) {
+    const {} = props
+
+    return (
+        
+    )
+}
+
+Bio.propTypes = {
+
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(memo(Bio))
+
