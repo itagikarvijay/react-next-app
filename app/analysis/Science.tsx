2c8fbac36f233d8028d222987edded1a4b4ab0eb
Vijay <itagikarv@gmail.com>
2026-07-10 18:41:33 +0530

    conflict code

---
+++ app/analysis/Science.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+interface Props {}
+
+function Science(props: Props) {
+    const {} = props
+
+    return (
+        
+    )
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Science)
